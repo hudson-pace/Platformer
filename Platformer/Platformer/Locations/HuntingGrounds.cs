64ea1cc80bdd496eac81e7614bdd4d17cb4ee301
Hudson Pace <roliatica@gmail.com>
2018-10-13 14:54:21 -0700

    Added vertical scrolling. Now an invibile barrier is formed around the edge of each location, including the bottom.

--- Platformer/Platformer/Locations/HuntingGrounds.cs
+++ Platformer/Platformer/Locations/HuntingGrounds.cs
@@ -19,6 +19,17 @@ namespace Platformer.Locations
             offsetX = 0;
             offsetY = 0;
             AddBorder();
+
+            Console.WriteLine(tiles.Length + ", " + tiles[0].Length);
+            tiles[3][28] = new Tile(3, 28, true, true);
+            tiles[4][25] = new Tile(4, 25, true, true);
+            tiles[5][22] = new Tile(5, 22, true, true);
+            tiles[6][19] = new Tile(6, 19, true, true);
+            tiles[7][16] = new Tile(7, 16, true, true);
+            tiles[8][13] = new Tile(8, 13, true, true);
+            tiles[9][10] = new Tile(9, 10, true, true);
+            tiles[10][7] = new Tile(10, 7, true, true);
+            tiles[11][4] = new Tile(11, 4, true, true);
         }
     }
 }
